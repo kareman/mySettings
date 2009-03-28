ac798ba1ddd983e744bed6b8031ff170c37dd5dd
Kare Morstol <kare@nottoobadsoftware.com>
2009-03-29 00:37:05 +0100

    LabeledPickerView: fixed a bug where the label for the third wheel would be invisible. added documentation.

--- Code/Generic classes/LabeledPickerView.h
+++ Code/Generic classes/LabeledPickerView.h
@@ -11,11 +11,16 @@
 
 #import <Foundation/Foundation.h>
 
-/** A picker view with labels under the selectionIndicator view */
+/** 
+ A picker view with labels under the selection indicator.
+ Similar to the one in the timer tab in the Clock app.
+ NB: has only been tested with less than four wheels. 
+ */
 @interface LabeledPickerView : UIPickerView {
 	NSMutableDictionary *labels;
 }
 
+/** Adds the label for the given component. */
 - (void) addLabel:(NSString *)labeltext forComponent:(NSUInteger)component;
 
 @end
