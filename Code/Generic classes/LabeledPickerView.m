2ff30349f1a8ce1b1438e54bfbdff940555d8e6a
Kare Morstol <kare@nottoobadsoftware.com>
2009-03-29 19:11:59 +0200

    Optimised LabeledPickerView. No longer adds empty labels.

--- Code/Generic classes/LabeledPickerView.m
+++ Code/Generic classes/LabeledPickerView.m
@@ -49,8 +49,8 @@
  The delegate is responsible for providing enough width for both the value and the label.
  */
 - (void)didMoveToWindow {
-	// exit if view is removed from the window.
-	if (!self.window)
+	// exit if view is removed from the window or there are no labels.
+	if (!self.window || [labels count] == 0)
 		return;
 	
 	UIFont *labelfont = [UIFont boldSystemFontOfSize:20];
@@ -70,43 +70,46 @@
 		// find the right side of the wheel
 		rightsideofwheel += [self rowSizeForComponent:component].width;
 		
+		// get the text for the label. 
+		// move on to the next if there is no label for this wheel.
 		NSString *text = [labels objectForKey:[NSNumber numberWithInt:component]];
-
-		// set up the frame for the label 
-		CGRect frame;
-		frame.size = [text sizeWithFont:labelfont];
-		// centre it vertically 
-		frame.origin.y = (self.frame.size.height / 2) - (frame.size.height / 2) - 0.5;
-		// align it to the right side of the wheel, with a margin.
-		// use a smaller margin for the rightmost wheel.
-		frame.origin.x = rightsideofwheel - frame.size.width - 
-						(component == self.numberOfComponents - 1 ? 5 : 7);
+		if (text) {
 			
-		// set up the label 
-		UILabel *label = [[[UILabel alloc] initWithFrame:frame] autorelease];
-		label.text = text;
-		label.font = labelfont;
-		label.backgroundColor = [UIColor clearColor];
-		label.shadowColor = [UIColor whiteColor];
-		label.shadowOffset = CGSizeMake(0,1);
+			// set up the frame for the label 
+			CGRect frame;
+			frame.size = [text sizeWithFont:labelfont];
+			// center it vertically 
+			frame.origin.y = (self.frame.size.height / 2) - (frame.size.height / 2) - 0.5;
+			// align it to the right side of the wheel, with a margin.
+			// use a smaller margin for the rightmost wheel.
+			frame.origin.x = rightsideofwheel - frame.size.width - 
+			(component == self.numberOfComponents - 1 ? 5 : 7);
 			
-		/* 
-		 and now for the tricky bit: adding the label to the view.
-		 a bit of a hack to be honest, might stop working if Apple decides to 
-		 change the inner workings of the UIPickerView.
-		 */	 
-		if (self.showsSelectionIndicator) { 
-			// if this is the last wheel, add label as the third view from the top
-			if (component==self.numberOfComponents-1) 
-				[self insertSubview:label atIndex:[self.subviews count]-3];
-			// otherwise add label as the 5th, 10th, 15th etc view from the top
-			else
-				[self insertSubview:label aboveSubview:[self.subviews objectAtIndex:5*(component+1)]];
-		} else
-			// there is no selection indicator, so just add it to the top
-			[self addSubview:label];
+			// set up the label 
+			UILabel *label = [[[UILabel alloc] initWithFrame:frame] autorelease];
+			label.text = text;
+			label.font = labelfont;
+			label.backgroundColor = [UIColor clearColor];
+			label.shadowColor = [UIColor whiteColor];
+			label.shadowOffset = CGSizeMake(0,1);
+			
+			/* 
+			 and now for the tricky bit: adding the label to the view.
+			 kind of a hack to be honest, might stop working if Apple decides to 
+			 change the inner workings of the UIPickerView.
+			 */	 
+			if (self.showsSelectionIndicator) { 
+				// if this is the last wheel, add label as the third view from the top
+				if (component==self.numberOfComponents-1) 
+					[self insertSubview:label atIndex:[self.subviews count]-3];
+				// otherwise add label as the 5th, 10th, 15th etc view from the top
+				else
+					[self insertSubview:label aboveSubview:[self.subviews objectAtIndex:5*(component+1)]];
+			} else
+				// there is no selection indicator, so just add it to the top
+				[self addSubview:label];
+		}
 	}
-	
 }
 
 @end
