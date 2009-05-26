6bde7f72a15ec62810bc847455a42fc48080379f
Kare Morstol <kare@nottoobadsoftware.com>
2009-05-26 19:40:33 +0200

    Documented SettingsDelegate

--- Code/Classes/SettingsDelegate.h
+++ Code/Classes/SettingsDelegate.h
@@ -19,10 +19,23 @@
 /** Called right after class has been initialised */
 - (void) cellDidInit:(SettingsCell *)cell;
 
+/** Called after all the changed settings have been saved.
+ 
+ @param savedsettings The settings that were saved.
+ */
 - (void) didSaveSettings:(NSDictionary *)savedsettings;
 
+/** Called when a custom cell is selected.
+ Useful when you need to know the index path of the cell that was selected.
+ 
+ @param indexpath The index path to the selected cell.
+ */
 - (void) customCellWasSelectedAtIndexPath:(NSIndexPath *)indexpath;
 
+/** Called right before the cell is returned from cellForRowAtIndexPath in SettingsMetadataSource.
+ Useful when you need to customise a cell based on where it is in the table view, 
+ like when the cells have alternating backgrounds.
+ */
 - (void) cellWillAppear:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexpath;
 
 @end
